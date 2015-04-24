230d8f98877bfa153b068a11e07ba119dcdc2471
Alex LaFroscia <alex@lafroscia.com>
2015-04-24 04:03:32 -0400

    Break JSON response out into separate function

--- routes/talk.go
+++ routes/talk.go
@@ -8,22 +8,23 @@ import (
 	"net/http"
 )
 
+func sendJSON(w http.ResponseWriter, obj interface{}) {
+	w.Header().Set("Content-Type", "application/vnd.api+json")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	json.NewEncoder(w).Encode(obj)
+}
+
 func TalkIndexRoute(w http.ResponseWriter, r *http.Request) {
 	talk1 := Talk{Id: "1", Title: "This is a talk"}
 	talk2 := Talk{Id: "2", Title: "This is a talk"}
 	talkarray := TalkCollection{Data: []Talk{talk1, talk2}}
-	w.Header().Set("Content-Type", "application/vnd.api+json")
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	json.NewEncoder(w).Encode(talkarray)
+	sendJSON(w, talkarray)
 }
 
 func TalkShowRoute(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 	talk := Talk{Id: id, Title: "This is a talk"}
 	talkWrapper := TalkResource{Data: talk}
-	w.Header().Set("Content-Type", "application/vnd.api+json")
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	json.NewEncoder(w).Encode(talkWrapper)
+	sendJSON(w, talkWrapper)
 }
